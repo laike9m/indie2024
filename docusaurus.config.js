9f69f0c438f8e72b62907846422476d3f6b23eee
laike9m <laike9m@gmail.com>
2024-12-31 15:34:35 -0800

    Use absolute URL for og image

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
       links: [],
       copyright: `Copyright © ${new Date().getFullYear()} laike9m`,
     },
-    image: 'img/ebook-cover.png', // 这将是你的默认社交媒体图片
+    image: 'https://indie2024.laike9m.com/img/ebook-cover.png', // 这将是你的默认社交媒体图片
     metadata: [
       // 基础 SEO
       { name: 'description', content: '2024 年，我踏上了独立开发之旅。这短短一年让我有很多体悟，因此想以年终总结的形式记录一下。' },
@@ -60,13 +60,13 @@ const config = {
       { property: 'og:title', content: '2024: 独立开发的第一年，我的成长与收获' },
       { property: 'og:description', content: '2024 年，我踏上了独立开发之旅。这短短一年让我有很多体悟，因此想以年终总结的形式记录一下。' },
       { property: 'og:type', content: 'website' },
-      { property: 'og:image', content: 'img/ebook-cover.png' },
+      { property: 'og:image', content: 'https://indie2024.laike9m.com/img/ebook-cover.png' },
 
       // Twitter
       { name: 'twitter:card', content: 'summary_large_image' },
       { name: 'twitter:title', content: '2024: 独立开发的第一年，我的成长与收获' },
       { name: 'twitter:description', content: '2024 年，我踏上了独立开发之旅。这短短一年让我有很多体悟，因此想以年终总结的形式记录一下。' },
-      { name: 'twitter:image', content: 'img/ebook-cover.png' },
+      { name: 'twitter:image', content: 'https://indie2024.laike9m.com/img/ebook-cover.png' },
     ],
   },
 };
