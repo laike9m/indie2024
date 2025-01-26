5e5153aa27368e72893269e48f6e958958bec8a6
laike9m <laike9m@gmail.com>
2025-01-25 21:41:37 -0800

    Small tweaks

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
       "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebar.js'),
+          sidebarPath: require.resolve("./sidebar.js"),
           routeBasePath: "/", // Makes docs the root
           breadcrumbs: false, // Removes breadcrumbs for a cleaner book look
         },
@@ -49,24 +49,42 @@ const config = {
     footer: {
       style: "dark",
       links: [],
-      copyright: `Copyright © ${new Date().getFullYear()} laike9m`,
+      copyright: `Copyright © ${new Date().getFullYear()} <a href="https://laike9m.com">laike9m</a>`,
     },
-    image: 'https://indie2024.laike9m.com/img/ebook-cover.png', // 这将是你的默认社交媒体图片
+    image: "https://indie2024.laike9m.com/img/ebook-cover.png", // 这将是你的默认社交媒体图片
     metadata: [
       // 基础 SEO
-      { name: 'description', content: '2024 年，我踏上了独立开发之旅。这短短一年让我有很多体悟，因此想以年终总结的形式记录一下。' },
+      {
+        name: "description",
+        content:
+          "2024 年，我踏上了独立开发之旅。这短短一年让我有很多体悟，因此想以年终总结的形式记录一下。",
+      },
 
       // Open Graph
-      { property: 'og:title', content: '2024: 独立开发的第一年，我的收获与成长' },
-      { property: 'og:description', content: '2024 年，我踏上了独立开发之旅。这短短一年让我有很多体悟，因此想以年终总结的形式记录一下。' },
-      { property: 'og:type', content: 'website' },
-      { property: 'og:image', content: 'https://indie2024.laike9m.com/img/ebook-cover.png' },
+      { property: "og:title", content: "2024: 独立开发的第一年，我的收获与成长" },
+      {
+        property: "og:description",
+        content:
+          "2024 年，我踏上了独立开发之旅。这短短一年让我有很多体悟，因此想以年终总结的形式记录一下。",
+      },
+      { property: "og:type", content: "website" },
+      {
+        property: "og:image",
+        content: "https://indie2024.laike9m.com/img/ebook-cover.png",
+      },
 
       // Twitter
-      { name: 'twitter:card', content: 'summary_large_image' },
-      { name: 'twitter:title', content: '2024: 独立开发的第一年，我的收获与成长' },
-      { name: 'twitter:description', content: '2024 年，我踏上了独立开发之旅。这短短一年让我有很多体悟，因此想以年终总结的形式记录一下。' },
-      { name: 'twitter:image', content: 'https://indie2024.laike9m.com/img/ebook-cover.png' },
+      { name: "twitter:card", content: "summary_large_image" },
+      { name: "twitter:title", content: "2024: 独立开发的第一年，我的收获与成长" },
+      {
+        name: "twitter:description",
+        content:
+          "2024 年，我踏上了独立开发之旅。这短短一年让我有很多体悟，因此想以年终总结的形式记录一下。",
+      },
+      {
+        name: "twitter:image",
+        content: "https://indie2024.laike9m.com/img/ebook-cover.png",
+      },
     ],
   },
 };
